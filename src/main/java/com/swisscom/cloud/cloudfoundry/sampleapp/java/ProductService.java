a87f765791d26c60b947b188aae56e003f4bf1f1
Tobi Fuhrimann <tobias.fuhrimann@gmail.com>
2018-02-28 12:29:24 +0100

    Exclude whole META-INF folder for Gradle

--- src/main/java/com/swisscom/cloud/cloudfoundry/sampleapp/java/ProductService.java
+++ src/main/java/com/swisscom/cloud/cloudfoundry/sampleapp/java/ProductService.java
@@ -10,9 +10,13 @@ import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
